b03f165bab0cc03e066828aa7fdc628e33edb41d
LathifNurHidayat <lathifnurhidayat09@gmail.com>
2025-02-05 01:17:09 +0700

    Helper

--- Sistem-Manajemen-Bengkel/SMB_Helper/CustomComponentHelper.cs
+++ Sistem-Manajemen-Bengkel/SMB_Helper/CustomComponentHelper.cs
@@ -4,6 +4,7 @@ using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Sistem_Manajemen_Bengkel.SMB_Helper
 {
@@ -21,6 +22,54 @@ namespace Sistem_Manajemen_Bengkel.SMB_Helper
             path.CloseAllFigures();
 
             panel.Region = new Region(path);
+        }
+
+
+        public static void CustomDataGrid(DataGridView grid)
+        {
+            DataGridView grid = grid;
+            grid.BackgroundColor = Color.White;
+
+            grid.EnableHeadersVisualStyles = false;
+            grid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+
+            // Mengatur ukuran font header kolom
+            grid.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 12, FontStyle.Bold);
+            grid.DefaultCellStyle.Font = new Font("Segoe UI", 12, FontStyle.Regular);
+            grid.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            grid.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            grid.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            // Mengatur warna header kolom
+            grid.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(52, 152, 219);
+            grid.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            grid.ColumnHeadersDefaultCellStyle.SelectionBackColor = Color.FromArgb(52, 152, 219);
+            grid.ColumnHeadersDefaultCellStyle.SelectionForeColor = Color.White;
+            grid.ForeColor = Color.DimGray;
+
+
+            // Menonaktifkan warna seleksi untuk sel
+            grid.DefaultCellStyle.SelectionBackColor = Color.FromArgb(240, 240, 240);
+            grid.DefaultCellStyle.SelectionForeColor = grid.DefaultCellStyle.ForeColor;
+
+            grid.ColumnHeadersHeight = 40;
+            grid.RowTemplate.Height = 55;
+
+            grid.RowHeadersVisible = false;
+
+            // Mencegah penggeseran kolom
+            grid.AllowUserToOrderColumns = false;
+
+            // Mencegah pengubahan ukuran kolom
+            grid.AllowUserToResizeColumns = true;
+
+            // Mencegah pengubahan ukuran baris
+            grid.AllowUserToResizeRows = false;
+
+            // Mencegah penambahan baris baru
+            grid.AllowUserToAddRows = false;
+
+            grid.ColumnHeadersDefaultCellStyle.Padding = new Padding(20, 0, 0, 0);
         }
     }
 }
