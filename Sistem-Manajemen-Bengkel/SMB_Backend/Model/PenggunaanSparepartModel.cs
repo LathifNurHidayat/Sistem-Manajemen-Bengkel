eb2d244cdc222ab0f59831a2a9bf733cbfbd5be2
LathifNurHidayat <lathifnurhidayat09@gmail.com>
2025-03-02 02:54:56 +0700

    Penggunaan Sparepart

---
+++ Sistem-Manajemen-Bengkel/SMB_Backend/Model/PenggunaanSparepartModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistem_Manajemen_Bengkel.SMB_Backend.Model
+{
+    public class PenggunaanSparepartModel
+    {
+        public int id_penggunaan_sparepart { get; set; }
+        public int id_sparepart { get; set; }
+        public string nama_sparepart { get; set; }
+        public int stok {  get; set; }
+        public int jumlah { get; set; }
+        public decimal harga { get; set; }
+        public decimal total_harga { get; set; }//harga as total_harga
+    }
+}
