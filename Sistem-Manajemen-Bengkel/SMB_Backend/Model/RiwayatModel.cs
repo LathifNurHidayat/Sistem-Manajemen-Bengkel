5fa45f6fac3084a87eb065350c9e534fcd8de7ff
LathifNurHidayat <lathifnurhidayat09@gmail.com>
2025-03-03 12:18:15 +0700

    Riwayat Model

--- Sistem-Manajemen-Bengkel/SMB_Backend/Model/RiwayatModel.cs
+++ Sistem-Manajemen-Bengkel/SMB_Backend/Model/RiwayatModel.cs
@@ -8,13 +8,18 @@ namespace Sistem_Manajemen_Bengkel.SMB_Backend.Model
 {
     public class RiwayatModel
     {
-        public int id_riwayat { get; set; }
-        public int id_jasa_servis { get; set; }
-        public string no_ktp_pelanggan { get; set; }
-        public string no_ktp_pegawai { get; set; }
-        public string no_ktp_mekanik { get; set; }
-        public int id_kendaraan { get; set; }
-        public int id_penggunaan_sparepart { get; set; }
+        public int id_riwayat { get; set; }//aa
+        public int id_jasa_servis { get; set; }//bb
+        public string no_ktp_pelanggan { get; set; }//cc
+        public string no_ktp_pegawai { get; set; }//dd
+        public string no_ktp_mekanik { get; set; }//ee
+        public int id_kendaraan { get; set; }//ff
+        public int id_penggunaan_sparepart { get; set; }//gg
+
+        public string jenis_servis { get; set; }
+        public string nama_pegawai { get; set; }
+        public string nama_mekanik { get; set; }
+        public string nama_sparepart { get; set; }
 
         public string nama_pelanggan { get; set; }
         public string no_polisi { get; set; }
@@ -27,7 +32,7 @@ namespace Sistem_Manajemen_Bengkel.SMB_Backend.Model
         public string keluhan { get; set; }
         public string catatan { get; set; }
         public decimal total_biaya { get; set; }
-        public string status { get; set; } //1 = Selesai, 2 = Dibatalkan
+        public int status { get; set; } //1 = Selesai, 2 = Dibatalkan
         public DateTime created_at { get; set; }
         public DateTime updated_at { get; set; }
     }
