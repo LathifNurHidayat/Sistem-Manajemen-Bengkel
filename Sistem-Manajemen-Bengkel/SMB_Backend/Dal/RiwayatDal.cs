5bd718cfb4ccd32c122db9480e7942df1598e14a
LathifNurHidayat <lathifnurhidayat09@gmail.com>
2025-03-04 12:18:50 +0700

    Riwayat Data Akses Layer

--- Sistem-Manajemen-Bengkel/SMB_Backend/Dal/RiwayatDal.cs
+++ Sistem-Manajemen-Bengkel/SMB_Backend/Dal/RiwayatDal.cs
@@ -12,40 +12,139 @@ namespace Sistem_Manajemen_Bengkel.SMB_Backend.Dal
 {
     public   class RiwayatDal
     {
-        public IEnumerable<BookingModel> ListData(string filter, object Dp)
+        /*  public IEnumerable<RiwayatModel> ListData(string filter, object Dp)
+          {
+              string sql = @$"SELECT 
+                          aa.id_riwayat, 
+                          bb.jenis_servis,
+                          COALESCE(cc.nama_pelanggan, aa.nama_pelanggan) AS nama_pelanggan,
+                          COALESCE(cc.no_ktp_pelanggan, aa.no_ktp_pelanggan) AS no_ktp_pelanggan,  
+                          COALESCE(ff.no_polisi, aa.no_polisi) AS no_polisi,       
+                          ISNULL(dd.nama_pegawai, '') AS nama_pegawai, 
+                          ISNULL(ee.nama_mekanik, '') AS nama_mekanik, 
+                          CASE 
+                              WHEN (aa.merek IS NULL OR aa.merek = '') AND (aa.kapasitas_mesin IS NULL OR aa.kapasitas_mesin = '')
+                              THEN CONCAT(ff.merek, ' ', ff.kapasitas_mesin, 'cc') 
+                              ELSE CONCAT(aa.merek, ' ', aa.kapasitas_mesin, 'cc') 
+                          END AS nama_kendaraan,
+                          aa.tanggal, 
+                          aa.keluhan, 
+                          aa.catatan, 
+                          aa.total_biaya, 
+                          aa.status
+                      FROM tb_riwayat aa
+                      LEFT JOIN tb_jasa_servis bb ON aa.id_jasa_servis = bb.id_jasa_servis
+                      LEFT JOIN tb_pelanggan cc ON aa.no_ktp_pelanggan = cc.no_ktp_pelanggan
+                      LEFT JOIN tb_pegawai dd ON aa.no_ktp_pegawai = dd.no_ktp_pegawai
+                      LEFT JOIN tb_mekanik ee ON aa.no_ktp_mekanik = ee.no_ktp_mekanik
+                      LEFT JOIN tb_kendaraan ff ON aa.id_kendaraan = ff.id_kendaraan
+                      LEFT JOIN tb_penggunaan_sparepart gg ON aa.id_penggunaan_sparepart = gg.id_penggunaan_sparepart
+                      LEFT JOIN tb_sparepart hh ON gg.id_sparepart = hh.id_sparepart
+                      {filter}
+                      GROUP BY 
+                          aa.id_riwayat, 
+                          bb.jenis_servis, 
+                          cc.nama_pelanggan, 
+                          cc.no_ktp_pelanggan, 
+                          ff.no_polisi,
+                          dd.nama_pegawai, 
+                          ee.nama_mekanik, 
+                          aa.merek, 
+                          aa.kapasitas_mesin, 
+                          ff.merek, 
+                          ff.kapasitas_mesin,
+                          aa.tanggal, 
+                          aa.keluhan, 
+                          aa.catatan, 
+                          aa.total_biaya, 
+                          aa.status, 
+                          aa.nama_pelanggan, 
+                          aa.no_ktp_pelanggan, 
+                          aa.no_polisi
+                      ORDER BY aa.tanggal DESC
+                      OFFSET @offset ROWS FETCH NEXT @fetch ROWS ONLY"; // Pastikan tidak ada spasi atau karakter tambahan
+
+              using var Conn = new SqlConnection(ConnStringHelper.GetConn());
+              return Conn.Query<RiwayatModel>(sql, Dp);
+          }*/
+
+        public IEnumerable<RiwayatModel> ListData(string filter, object Dp)
         {
             string sql = @$"SELECT 
-                                aa.id_booking,
-                                COALESCE (bb.nama_pelanggan, aa.nama_pelanggan) AS nama_pelanggan,
-                                COALESCE (dd.no_polisi, aa.no_polisi) AS no_polisi,
-
-                            CASE 
-                                WHEN (aa.merek IS NULL  OR aa.merek = '') AND (aa.kapasitas_mesin IS NULL OR aa.kapasitas_mesin = '')
-                                THEN CONCAT (dd.merek , ' ' , dd.kapasitas_mesin, 'cc')
-                                ELSE CONCAT (aa.merek , ' ' , aa.kapasitas_mesin, 'cc')
-                            END AS nama_kendaraan,
-
-                                COALESCE (dd.transmisi, aa.transmisi) AS transmisi,
-                                ISNULL (ee.jenis_servis, '') AS jenis_servis,
-                                aa.tanggal, aa.antrean, aa.keluhan, aa.status
-                            FROM 
-                                tb_booking aa
-
-                            LEFT JOIN tb_pelanggan bb 
-                                ON aa.no_ktp_pelanggan = bb.no_ktp_pelanggan
-                            LEFT JOIN tb_kendaraan dd 
-                                ON aa.id_kendaraan = dd.id_kendaraan
-                            LEFT JOIN tb_jasa_servis ee 
-                                ON aa.id_jasa_servis = ee.id_jasa_servis
-
-                            WHERE
-                                {filter}
-                            ORDER BY 
-                                aa.antrean ASC
-                            OFFSET @offset ROWS FETCH NEXT @fetch ROWS ONLY";
+                        bb.jenis_servis,
+                        COALESCE(cc.nama_pelanggan, aa.nama_pelanggan) AS nama_pelanggan,
+                        COALESCE(cc.no_ktp_pelanggan, aa.no_ktp_pelanggan) AS no_ktp_pelanggan,  
+                        COALESCE(ff.no_polisi, aa.no_polisi) AS no_polisi,       
+                        ISNULL(dd.nama_pegawai, '') AS nama_pegawai, 
+                        ISNULL(ee.nama_mekanik, '') AS nama_mekanik, 
+                        CASE 
+                            WHEN (aa.merek IS NULL OR aa.merek = '') AND (aa.kapasitas_mesin IS NULL OR aa.kapasitas_mesin = '')
+                            THEN CONCAT(ff.merek, ' ', ff.kapasitas_mesin, 'cc') 
+                            ELSE CONCAT(aa.merek, ' ', aa.kapasitas_mesin, 'cc') 
+                        END AS nama_kendaraan,
+                        aa.tanggal, 
+                        aa.keluhan, 
+                        aa.catatan, 
+                        aa.total_biaya, 
+                        aa.status
+                    FROM tb_riwayat aa
+                    LEFT JOIN tb_jasa_servis bb 
+                        ON aa.id_jasa_servis = bb.id_jasa_servis
+                    LEFT JOIN tb_pelanggan cc
+                        ON aa.no_ktp_pelanggan = cc.no_ktp_pelanggan
+                    LEFT JOIN tb_pegawai dd
+                        ON aa.no_ktp_pegawai = dd.no_ktp_pegawai
+                    LEFT JOIN tb_mekanik ee 
+                        ON aa.no_ktp_mekanik = ee.no_ktp_mekanik
+                    LEFT JOIN tb_kendaraan ff
+                        ON aa.id_kendaraan = ff.id_kendaraan
+                    {filter}
+                    GROUP BY 
+                        aa.id_riwayat, 
+                        bb.jenis_servis, 
+                        cc.nama_pelanggan, 
+                        cc.no_ktp_pelanggan, 
+                        ff.no_polisi,
+                        dd.nama_pegawai, 
+                        ee.nama_mekanik, 
+                        aa.merek, 
+                        aa.kapasitas_mesin, 
+                        ff.merek, 
+                        ff.kapasitas_mesin,
+                        aa.tanggal, 
+                        aa.keluhan, 
+                        aa.catatan, 
+                        aa.total_biaya, 
+                        aa.status,
+                        aa.nama_pelanggan, 
+                        aa.no_ktp_pelanggan, 
+                        aa.no_polisi
+                    ORDER BY 
+                        aa.tanggal DESC
+                    OFFSET @offset ROWS FETCH NEXT @fetch ROWS ONLY";
             using var Conn = new SqlConnection(ConnStringHelper.GetConn());
-            return Conn.Query<BookingModel>(sql, Dp);
+            return Conn.Query<RiwayatModel>(sql, Dp);
         }
 
+
+        public int CountData(string filter, object Dp)
+        {
+            string sql = @$"SELECT 
+                                COUNT(aa.id_riwayat)
+                            FROM tb_riwayat aa
+                            LEFT JOIN tb_jasa_servis bb 
+                                ON aa.id_jasa_servis = bb.id_jasa_servis
+                            LEFT JOIN tb_pelanggan cc
+                                ON aa.no_ktp_pelanggan = cc.no_ktp_pelanggan
+                            LEFT JOIN tb_pegawai dd
+                                ON aa.no_ktp_pegawai = dd.no_ktp_pegawai
+                            LEFT JOIN tb_mekanik ee 
+                                ON aa.no_ktp_mekanik = ee.no_ktp_mekanik
+                            LEFT JOIN tb_kendaraan ff
+                                ON aa.id_kendaraan = ff.id_kendaraan
+                                {filter}";
+            using var Conn = new SqlConnection(ConnStringHelper.GetConn());
+            return Conn.ExecuteScalar<int>(sql, Dp);
+        }
     }
 }
