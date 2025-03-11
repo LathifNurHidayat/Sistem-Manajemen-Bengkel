6229411f4a99c8e53157409557bed9ed1eb63c8c
LathifNurHidayat <lathifnurhidayat09@gmail.com>
2025-03-12 06:21:19 +0700

    Startup Form

---
+++ Sistem-Manajemen-Bengkel/SMB_Form/StartupForm/StartupForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Sistem_Manajemen_Bengkel.SMB_Form.StartupForm
+{
+    partial class StartupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartupForm));
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top;
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft New Tai Lue", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(236, 68);
+            label1.Name = "label1";
+            label1.Size = new Size(472, 58);
+            label1.TabIndex = 0;
+            label1.Text = "Aplikasi Booking Servis";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(370, 193);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(210, 178);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top;
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft New Tai Lue", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.DimGray;
+            label2.Location = new Point(389, 480);
+            label2.Name = "label2";
+            label2.Size = new Size(166, 26);
+            label2.TabIndex = 2;
+            label2.Text = "@2025 Lathif.Dev";
+            // 
+            // StartupForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(945, 534);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "StartupForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+    }
+}
---
+++ Sistem-Manajemen-Bengkel/SMB_Form/StartupForm/StartupForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sistem_Manajemen_Bengkel.SMB_Form.Pelanggan.MainMenuForm;
+
+namespace Sistem_Manajemen_Bengkel.SMB_Form.StartupForm
+{
+    public partial class StartupForm : Form
+    {
+        private System.Windows.Forms.Timer _opacityTimer;
+        private System.Windows.Forms.Timer _delayTimer;
+        public static Form _startupForm = new StartupForm();
+
+        public StartupForm()
+        {
+            InitializeComponent();
+            _startupForm = this;
+
+            this.Opacity = 0;
+            this._opacityTimer = new System.Windows.Forms.Timer();
+            this._delayTimer = new System.Windows.Forms.Timer();
+
+            this.Load += LoadForm_Load;
+            this._opacityTimer.Interval = 10;
+            this._opacityTimer.Tick += _opacityTimer_Tick;
+            this._delayTimer.Interval = 2000;
+            this._delayTimer.Tick += _delayTimer_Tick;
+        }
+
+        private void LoadForm_Load(object? sender, EventArgs e)
+        {
+            this._opacityTimer.Start();
+        }
+
+        private void _opacityTimer_Tick(object? sender, EventArgs e)
+        {
+            if (this.Opacity < 1)
+            {
+                this.Opacity += 0.05;
+            }
+            else
+            {
+                this._opacityTimer.Stop();
+                this._delayTimer.Start();
+            }
+        }
+
+        private void _delayTimer_Tick(object? sender, EventArgs e)
+        {
+            this._delayTimer.Stop();
+            MainMenuFirst mainForm = new MainMenuFirst();
+            mainForm.WindowState = FormWindowState.Maximized;
+            mainForm.Show();
+            this.Hide();
+        }
+    }
+}
